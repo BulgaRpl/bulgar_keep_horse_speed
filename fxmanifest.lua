29e187099c9bbf75720c7d4eec2b12b9f8b22e54
BulgaRpl <69915160+BulgaRpl@users.noreply.github.com>
2026-04-22 22:29:50 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,9 @@
 fx_version 'adamant'
+
 games { 'rdr3' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+lua54 'yes'
 
 author 'BulgaR' -- https://discord.gg/dcyY2e5kb3
 description 'BulgaR Keep Horse Speed & Disable Jump From Horse on Horses or Wagons'
